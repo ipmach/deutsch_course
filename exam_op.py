487b00e22e86a6f9f1dd6746c37b0ade34cad9ab
angre.garcia-gomez@ait.ac.at <angre.garcia-gomez@ait.ac.at>
2025-03-04 19:31:13 +0100

    ADD exam operations Noun

---
+++ exam_op.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+
+import db.schema as sc
+import typing as t
+
+
+@dataclass
+class NounResults:
+    article_is_correct: bool = False
+    german_is_correct: bool = False
+    plural_is_correct: bool = False
+
+    points: int = 0
+    max_points: int = 3
+
+
+@dataclass
+class ExamQuestion:
+    title: str
+    parts: t.List[str]
+
+
+def noun_question(elem: sc.NounSchema) -> ExamQuestion:
+    return ExamQuestion(
+        title=f"Translate the word: {elem.english_word}",
+        parts=["word", "article", "plural"]
+    )
+
+
+def noun_check_answer(
+    elem: sc.NounSchema, answer: t.Dict[str, str]
+) -> NounResults:
+    return NounResults(
+        article_is_correct=(a := answer["article"] == elem.article),
+        german_is_correct=(b := answer["word"] == elem.german_word),
+        plural_is_correct=(c := answer["plural"] == elem.plural_word),
+        points=int(a) + int(b) + int(c)
+    )
+    
+
+
---
+++ test/test_exam_op.py
@@ -0,0 +1,59 @@
+
+import db.schema as sc
+import exam_op as exam
+
+import unittest
+
+
+class ExamTestCase(unittest.TestCase):
+    def setUp(self):
+        self.noun = sc.NounSchema(
+            english_word="hi",
+            german_word="hallo",
+            plural_word="hallos",
+            article="das",
+        )
+        
+    def test_noun_question(self) -> None:
+        question = exam.noun_question(self.noun)
+
+        self.assertEqual(question.title, f"Translate the word: hi")
+        self.assertListEqual(
+            question.parts, ["word", "article", "plural"]
+        )
+
+    def test_one_answer_incorrect(self) -> None:
+        result = exam.noun_check_answer(
+            self.noun, {
+                "word": "hallo", "plural": "hallos", "article": "die"
+            }
+        )
+
+        self.assertTrue(result.german_is_correct)
+        self.assertTrue(result.plural_is_correct)
+        self.assertFalse(result.article_is_correct)
+        self.assertEqual(result.points, 2)
+
+    def test_two_answer_incorrect(self) -> None:
+        result = exam.noun_check_answer(
+            self.noun, {
+                "word": "hallos", "plural": "hallos", "article": "die"
+            }
+        )
+
+        self.assertFalse(result.german_is_correct)
+        self.assertTrue(result.plural_is_correct)
+        self.assertFalse(result.article_is_correct)
+        self.assertEqual(result.points, 1)
+
+    def test_all_answer_correct(self) -> None:
+        result = exam.noun_check_answer(
+            self.noun, {
+                "word": "hallo", "plural": "hallos", "article": "das"
+            }
+        )
+
+        self.assertTrue(result.german_is_correct)
+        self.assertTrue(result.plural_is_correct)
+        self.assertTrue(result.article_is_correct)
+        self.assertEqual(result.points, 3)
