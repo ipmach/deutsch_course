9f59caf55445cccbedc49649e656d9df751bb551
angre.garcia-gomez@ait.ac.at <angre.garcia-gomez@ait.ac.at>
2025-03-28 19:28:51 +0100

    ADD next command

--- main.py
+++ main.py
@@ -10,18 +10,23 @@ db = NounDB()
 
 try:
     print(f"Start with seed {seed}")
-    for elem in db.sample(len(db), seed=seed):
+    for j, elem in enumerate(db.sample(len(db), seed=seed)):
 
         is_correct = False 
         while not is_correct:
             print("#######################################")
+            print(f"[{j + 1}/{len(db)}] ", end="")
             question = exam.noun_question(elem) 
             print(question.title)
 
             answer = {}
+            skip = False
             for task in question.parts:
-                answer[task] = input(f"  - {task}: ")
-
+                input_ = "" if skip else input(f"  - {task}: ") 
+                if input_ == "nächste":
+                    skip = True
+                answer[task] = input_ 
+                
             print("")
             print(results := exam.noun_check_answer(elem, answer=answer))
             print("#######################################")
